dd2f6f10e8566b8bf6606388ae0bfa0ea0ae0e60
Austin Connelly <ajjconnelly@gmail.com>
2014-09-17 12:08:27 -0700

    sendModal changes

--- client/send/send.controller.js
+++ client/send/send.controller.js
@@ -3,15 +3,29 @@ angular.module('bitclip.send', [
 ])
 
 .controller('sendController', function($scope) {
+  
+
   $scope.send = {
     closeEl: '.close',
     modal: {
       templateUrl: 'send/send.btn.html',
       position: {
-        top: '25%',
-        left: '25%'
+        top: '90%',
+        left: '0%'
       },
       fade: false
     }
   };
+
+  
+  
+  //sendPayment Method
+  
+  $scope.sendPayment = function(amount){  
+    console.log('hello worldz', $scope.amount);
+
+
+  }
+
+
 });

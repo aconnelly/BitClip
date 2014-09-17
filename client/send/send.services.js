cabc5fc9b662371454b512b19f0f0c9eb14a4565
Austin Connelly <ajjconnelly@gmail.com>
2014-09-17 15:36:19 -0700

    perstistentTransaction factory, morph-modal functionality

--- client/send/send.services.js
+++ client/send/send.services.js
@@ -1,5 +1,26 @@
 //not loaded into app.js yet
 
-angular.module('bitclip.send', [
+angular.module('bitclip.sendService', [
   'ui.router'
 ])
+
+.factory('persistentTransaction', function(){
+  
+  //maintain transaction details
+  var transactionDetails = {
+    amount:0,
+    destination: undefined
+  };
+  
+  //update transaction details with passed in object
+  var updateTransaction = function(transactionObj){
+    transactionDetails = transactionObj;
+  }
+
+  //TODO: sendPayment logic
+
+  return {
+    transactionDetails: transactionDetails,
+    updateTransaction: updateTransaction
+  };
+})
